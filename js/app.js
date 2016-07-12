6a05078ec0f4287a009c015003d4a7b0975d39f5
Dmitry Rudiuk <dmitry.rudiuk@gmail.com>
2016-07-13 02:33:42 +0300

    create

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	// ADD SLIDEDOWN ANIMATION TO DROPDOWN //
+  $('.dropdown').on('show.bs.dropdown', function(e){
+  	$('.dropdown-menu').stop(true, true).slideUp(150);
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown(250);
+     $(this).find("second-drop").first().click(function(){
+     	$( "second-drop" ).toggle( "slide" )
+     });
+  });
+
+  // ADD SLIDEUP ANIMATION TO DROPDOWN //
+  $('.dropdown').on('hide.bs.dropdown', function(e){
+    $(this).find('.dropdown-menu').stop(true, true).slideUp(0);
+  });
+
+
+
+});
